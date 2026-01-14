e65afb80f98053fb0c81d17e26d7655bf5544750
tonyUchi <tony_lopez014@hotmail.com>
2026-01-14 13:07:33 -0600

    creacion de asistente con persistencia

---
+++ app_asistente.py
@@ -0,0 +1,80 @@
+import datetime
+import json
+import os
+import uuid 
+
+ARCHIVO_JSON = "log_sesiones.json"
+
+def guardar_log_json(session_id, usuario_msg, respuesta_asistente):
+    """Guarda la conversación en un archivo JSON con marca de tiempo y sesión."""
+    fecha_hora = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    entrada = {
+        "timestamp": fecha_hora,
+        "usuario": usuario_msg,
+        "asistente": respuesta_asistente
+    }
+
+
+    if os.path.exists(ARCHIVO_JSON):
+        with open(ARCHIVO_JSON, "r", encoding="utf-8") as f:
+            data = json.load(f)
+    else:
+        data = {}
+
+    if session_id not in data:
+        data[session_id] = []
+
+    data[session_id].append(entrada)
+
+    with open(ARCHIVO_JSON, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
+def cargar_sesion(session_id):
+    if os.path.exists(ARCHIVO_JSON):
+        with open(ARCHIVO_JSON, "r", encoding="utf-8") as f:
+            data = json.load(f)
+        if session_id in data:
+            print("interacción pasada")
+            for entrada in data[session_id]:
+                print(f"[{entrada['timestamp']}] Usuario: {entrada['usuario']}")
+                print(f"[{entrada['timestamp']}] Asistente: {entrada['asistente']}")
+            print("-" * 50)
+        else:
+            print("--- Nueva sesión iniciada ---")
+    else:
+        print("--- No hay sesiones previas ---")
+
+
+#aqui defino las reglas, pero ya que no me pidieron muchas, solo puse las que ustedes me propusieron
+def asistente_soporte():
+    reglas = {
+        "hola": "¡Hola! Soy tu asistente de soporte. ¿Cómo puedo ayudarte?",
+        "consulta": "¿En qué puedo ayudarte con tu consulta?",
+        "problema": "¿Que problema estas presentando?",
+        "adios": "¡Hasta pronto!"
+    }
+
+    session_id = str(uuid.uuid4())
+
+    print(f"--- Sesión: {session_id}) ---")
+    cargar_sesion(session_id)
+
+    while True:
+        entrada = input("\nUsuario: ")
+        usuario_msg = entrada.lower().strip()
+
+        if usuario_msg in ["salir"]:
+            print("Asistente: Disculpa si no pude ayudarte!!!")
+            break
+
+        respuesta = "Lo siento solo soy un asistente de prueba para tech company, no puedo ayudarte mas."
+        for palabra, msg_soporte in reglas.items():
+            if palabra in usuario_msg:
+                respuesta = msg_soporte
+                break
+
+        print(f"Asistente: {respuesta}")
+        guardar_log_json(session_id, entrada, respuesta)
+
+if __name__ == "__main__":
+    asistente_soporte()
